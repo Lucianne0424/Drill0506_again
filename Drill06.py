20da061a78aac9dc1c9f2dd3680d801e0eb2d134
Lucianne0424 <pwj0424@naver.com>
2023-10-04 18:30:35 +0900

    목표점들을 차례로 이동하는 소년 구현 완료.

--- Drill06.py
+++ Drill06.py
@@ -47,14 +47,20 @@ def set_new_target_arrow():
     global sx, sy, hx, hy, t
     global action
     global frame
+    global target_exists
 
-    sx, sy = cx, cy
-    # hx, hy = 50,50
-    hx, hy = points[0]
-    t = 0.0
-    action = 1 if sx < hx else 0
-    frame = 0
-
+    if points:
+        sx, sy = cx, cy
+        # hx, hy = 50,50
+        hx, hy = points[0]
+        t = 0.0
+        action = 1 if sx < hx else 0
+        frame = 0
+        target_exists = True
+    else:
+        action = 3 if action == 1 else 2
+        frame = 0
+        target_exists = False
 
 def render_world():
     clear_canvas()
@@ -72,14 +78,15 @@ def update_world():
     global t
 
     frame = (frame + 1) % 8
-
-    if t <= 1.0:
-        cx = (1 - t) * sx + t * hx
-        cy = (1 - t) * sy + t * hy
-        t += 0.001
-    else:
-        cx, cy = hx, hy
-        set_new_target_arrow()
+    if target_exists:
+        if t <= 1.0:
+            cx = (1 - t) * sx + t * hx
+            cy = (1 - t) * sy + t * hy
+            t += 0.001
+        else:
+            cx, cy = hx, hy
+            del points[0]
+            set_new_target_arrow()
 
 
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
